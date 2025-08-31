b7ed611d4b3493fd07262ad35c4c7ecbb44bc4a2
K1LL3RKR3W <144093387+K1LL3RKR3W@users.noreply.github.com>
2025-08-31 01:29:05 -0400

    Update vaft.js

--- vaft.js
+++ vaft.js
@@ -159,7 +159,7 @@ twitch-videoad.js text/javascript
                 `;
                 super(URL.createObjectURL(new Blob([newBlobStr])), options);
                 twitchWorkers.push(this);
-                console.log("Creating new AdCheck Var");
+                console.log("Creating new AdCheck var");
                 var adStarted = true;
                 this.addEventListener('message', (e) => {
                     if (e.data.key == 'ShowAdBlockBanner') {
@@ -169,10 +169,11 @@ twitch-videoad.js text/javascript
                         adBlockDiv.P.textContent = 'Blocking ads';
                         adBlockDiv.style.display = 'block';
                         if (adStarted == true) {
-                            console.log("BLOCKING ADS???");
+                            console.log("BLOCKING ADS");
                             doTwitchPlayerTask(true, false, false, false, false);
                             adStarted = false;
-                            console.log(adStarted);
+                            console.log(adStarted)
+                        }
                     } else if (e.data.key == 'HideAdBlockBanner') {
                         if (adBlockDiv == null) {
                             adBlockDiv = getAdBlockDiv();
@@ -180,7 +181,7 @@ twitch-videoad.js text/javascript
                         adBlockDiv.style.display = 'none';
                         console.log("NOT BLOCKING ADS");
                         adStarted = true;
-                        console.log(adStarted);
+                        console.log(adStarted)
                     } else if (e.data.key == 'PauseResumePlayer') {
                         doTwitchPlayerTask(true, false, false, false, false);
                     } else if (e.data.key == 'ForceChangeQuality') {
